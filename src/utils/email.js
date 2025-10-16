7f61a848b9f37c86e6bcab797216d75e9601cef4
Jomelgar <jomelgar1903@gmail.com>
2025-10-15 23:07:54 -0600

    EmailJS -> PowerAutomate

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,14 +1,12 @@
 import emailjs from 'emailjs-com';
 
-export function sendVerificationCode(email, code) {
-  const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-  const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID_VERIFICATION;
-  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-
-  const params = {
+export async function sendVerificationCode(email, code) {
+  return await fetch(import.meta.env.VITE_URL_CODE, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
     email: email,
-    verification_code: code,
-  };
-
-  return emailjs.send(serviceId, templateId, params, publicKey);
+    verification_code: code
+  })
+});
 }
